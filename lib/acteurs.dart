170097b3f50781851dd47b9bcbff89dbf9baea69
Jeremie Gavin <gavin1u@etu.univ-lorraine.fr>
2024-06-12 10:08:36 +0200

    Creation de la class acteur

--- lib/acteurs.dart
+++ lib/acteurs.dart
@@ -2,14 +2,15 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'models/acteurs.dart';
 
 class Acteurs extends StatelessWidget {
   const Acteurs({super.key});
 
-  final List<String> listeActeurs = const ["Acteur 1", "Acteur 2", "Acteur 3"];
+  // final List<String> listeActeurs = const ["Acteur 1", "Acteur 2", "Acteur 3"];
 
-  Future<List<String>> fetchActeurs() async {
-    //return ["Acteur A", "Acteur B", "Acteur C"];
+  Future<List<Acteur>> fetchActeurs() async {
+    // return ["Acteur A", "Acteur B", "Acteur C"];
 
     final reponse = await http.get(
         Uri.parse(
@@ -23,9 +24,9 @@ class Acteurs extends StatelessWidget {
     if (reponse.statusCode == 200) {
       List<dynamic> data = json.decode(reponse.body);
       print(reponse.body);
-      return data.map((elt) => elt["nom"].toString()).toList();
+      return data.map((elt) => Acteur.fromJson(elt)).toList();
     } else {
-      return ["ERREUR API"];
+      return [Acteur(personneId: "0", nom: "ERREUR", nbFilm: 0)];
     }
   }
 
@@ -33,7 +34,7 @@ class Acteurs extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Acteurs")),
-      body: FutureBuilder<List<String>>(
+      body: FutureBuilder<List<Acteur>>(
           future: fetchActeurs(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -44,7 +45,10 @@ class Acteurs extends StatelessWidget {
               return ListView.builder(
                 itemCount: snapshot.data!.length,
                 itemBuilder: (context, index) {
-                  return ListTile(title: Text(snapshot.data![index]));
+                  return ListTile(
+                    title: Text(snapshot.data![index].nom),
+                    subtitle: Text(snapshot.data![index].age.toString()),
+                  );
                 },
               );
             }
